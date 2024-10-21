b21e792b42ffee1e8677c3ffb52c11c1f40ebc16
Kevin6969696969 <160507577+Kevin6969696969@users.noreply.github.com>
2024-10-21 00:09:59 -0300

    Add files via upload

--- graficos.js
+++ graficos.js
@@ -1,62 +1,89 @@
-//grafico de piza
-const ctxPizza = document.getElementById('graficoPizza').getContext('2');
-const graficoPizza = new Chart(ctxPizza, (
-    type:'pie',
+// Gráfico de Pizza 1
+const ctxPizza1 = document.getElementById('graficoPizza1').getContext('2d');
+const graficoPizza1 = new Chart(ctxPizza1, {
+    type: 'pie',
     data: {
-        labels: ['Aaaaaa', 'dhbuhdb'],
-        daaet[{
-            label;'Profissões os alunos',
-            data [45, 30, 15, 10],
-            bakrounColor; [
-                '#F05454', '#30475E', '#DDDDDD', '#222831'
+        labels: ['Sim', 'Não'],
+        datasets: [{
+            label: 'Resposta dos Alunos',
+            data: [15, 6],
+            backgroundColor: [
+                '#F05454', '#46298f', '#DDDDDD', '#222831'
             ],
-            borderColor: '#DDDDDD' ,
+            borderColor: '#DDDDDD',
             borderWidth: 1
         }]
     },
-    options: [
+    options: {
         responsive: true,
-        plugins: [
+        plugins: {
             legend: {
-                position; 'bottom'.
+                position: 'bottom'
             },
             title: {
                 display: true,
-                text: 'Paticipação nas Redes Sociais'
-            }
-        ]
-    ]
-));
-
-// Gráico de Barras
-const ctxBarra = document.getElementById('graficoBarra').getContext('2d');
-const graficoBarra = new Chart(ctxBarra, {
-    type: 'bar',
-    data; {
-        labels: ['fijbhifbi', 'uhabuahf'],
-        datasets: [[
-            label; 'Usuários ativos (em milhões)',
-            data: [2800, 1500],
-            backgroundColor: '#F05454',
-            borderColor; '#30475E',
-            borderWidth; 1
-        ]]
-    },
-    options: [
-        responsive: true,
-        plugins: [
-            legen; [
-                position: 'top',
-            ],
-            title: [
-                display: true,
-                text: 'Usuários Ativos nas Redes Sociais (milhões)'
-            ]
-        ],
-        scales; {
-            y; {
-                beginAtZero: true
+                text: 'Você já trabalha em um emprego que envolve a sua profissão?'
             }
         }
-    ]
+    }
+});
+
+// Gráfico de Pizza 2
+const ctxPizza2 = document.getElementById('graficoPizza2').getContext('2d');
+const graficoPizza2 = new Chart(ctxPizza2, {
+    type: 'pie',
+    data: {
+        labels: ['Sim', 'Não'],
+        datasets: [{
+            label: 'Resposta dos Alunos',
+            data: [19, 2],
+            backgroundColor: [
+                '#F05454', '#46298f', '#DDDDDD', '#222831'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom'
+            },
+            title: {
+                display: true,
+                text: 'Você já pensou em como pode chegar nessa profissão que deseja?'
+            }
+        }
+    }
+});
+
+// Gráfico de Pizza 3
+const ctxPizza3 = document.getElementById('graficoPizza3').getContext('2d');
+const graficoPizza3 = new Chart(ctxPizza3, {
+    type: 'pie',
+    data: {
+        labels: ['Sim', 'Não'],
+        datasets: [{
+            label: 'Resposta dos Alunos',
+            data: [16, 5],
+            backgroundColor: [
+                '#F05454', '#46298f', '#DDDDDD', '#222831'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom'
+            },
+            title: {
+                display: true,
+                text: 'Precisa de ensino superior para essa profissão?'
+            }
+        }
+    }
 });
