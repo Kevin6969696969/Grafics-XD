a5a8dcbd0b2aca4f77efb769ccebc3842fc6cf08
Kevin6969696969 <160507577+Kevin6969696969@users.noreply.github.com>
2024-10-22 08:57:52 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # Grafics-XD
-
-
-'medicina', 'Medicina Veterinária', 'Médico Infectologista', 'Técnico de segurança', 'Programador', 'pedreiro', 'Farmacêutico bioquímico', 'engenharia mecânica', 'Ed Fisica', 'agronomia' 
-
-1, 2, 1, 1, 2, 1, 1, 3, 1, 1
--- graficos.js
+++ graficos.js
@@ -1,3 +1,63 @@
+// Gráfico de Barra1
+const ctxBarra1 = document.getElementById('graficoBarra1').getContext('2d');
+const graficoBarra1 = new Chart(ctxBarra1, {
+    type: 'pie',
+    data: {
+        labels: ['medicina', 'Medicina Veterinária', 'Médico Infectologista', 'Técnico de segurança', 'Programador', 'pedreiro', 'Farmacêutico bioquímico', 'engenharia mecânica', 'Ed Fisica', 'agronomia' ],
+        datasets: [{
+            label: 'Resposta dos Alunos',
+            data: [1, 2, 1, 1, 2, 1, 1, 3, 1, 1],
+            backgroundColor: [
+                '#F05454', '#46298f', '#2cb5b8', '#311c91', '#b827b8', '#27b844', '#a39f4e', '#91b5db', '#ff4093', '#c6ff40'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom'
+            },
+            title: {
+                display: true,
+                text: 'Qual profissão você quer seguir na sua carreira?'
+            }
+        }
+    }
+});
+
+// Gráfico de Barra2
+const ctxBarra2 = document.getElementById('graficoBarra2').getContext('2d');
+const graficoBarra2 = new Chart(ctxBarra2, {
+    type: 'bar',
+    data: {
+        labels: ['Sim', 'Não', 'teu pai'],
+        datasets: [{        
+            label: 'Resposta dos Alunos',
+            data: [16, 5, 69],
+            backgroundColor: [
+                '#F05454', '#46298f', '#DDDDDD', '#222831'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top'
+            },
+            title: {
+                display: true,
+                text: 'Precisa de ensino superior para essa profissão?'
+            }
+        }
+    }
+});
+
 // Gráfico de Pizza 1
 const ctxPizza1 = document.getElementById('graficoPizza1').getContext('2d');
 const graficoPizza1 = new Chart(ctxPizza1, {
